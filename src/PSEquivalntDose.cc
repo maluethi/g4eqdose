bc7cfebd29818d07832ba4c33841aeea8d85e741
Matthias Luethi <matthias.luethi@metas.ch>
2020-12-18 10:51:42 +0100

    add handling of different particles

--- src/PSEquivalntDose.cc
+++ src/PSEquivalntDose.cc
@@ -12,7 +12,14 @@ class ConvCoeff;
 PSEquivalntDose::PSEquivalntDose(G4String name, G4int depth) :
     G4VPrimitiveScorer(name, depth), G4UImessenger(), HCID(-1),EvtMap(0),weighted(true) {
 
-    fGammaCoeff = ConvCoeff("bla", "bla", "data/");
+    G4String data_dir = "data/";
+    G4String geometry = "AP";
+
+    fGammaCoeff = ConvCoeff("Photons", geometry, data_dir);
+    fElectronCoeff = ConvCoeff("Electrons", geometry, data_dir);
+    fPositronCoeff = ConvCoeff("Positrons", geometry, data_dir);
+    fProtonCoeff = ConvCoeff("Protons", geometry, data_dir);
+    fNeutronCoeff = ConvCoeff("Neutrons", geometry, data_dir);
 
 }
 
@@ -27,39 +34,60 @@ void PSEquivalntDose::Initialize(G4HCofThisEvent* HCE) {
 
 }
 
-void PSEquivalntDose::EndOfEvent(G4HCofThisEvent*) {};
+void PSEquivalntDose::EndOfEvent(G4HCofThisEvent*) {}
 
 G4bool PSEquivalntDose::ProcessHits(G4Step * aStep, G4TouchableHistory *) {
-    G4cout << "HIT HIT HIT" << G4endl;
+    //G4cout << "HIT HIT HIT" << G4endl;
 
     G4double stepLength = aStep->GetStepLength();
     if ( stepLength == 0. ) return FALSE;
 
     auto pre_step = aStep->GetPreStepPoint();
     auto energy = pre_step->GetKineticEnergy();
+    auto pid = aStep->GetTrack()->GetParticleDefinition()->GetPDGEncoding();
+    auto name = aStep->GetTrack()->GetParticleDefinition()->GetParticleName();
 
-    auto coeff = fGammaCoeff.GetCoefficient(energy);
+    G4double coeff = 0;
+
+    switch (pid) {
+        case 22:
+            coeff = fGammaCoeff.GetCoefficient(energy); break;
+        case 11:
+            coeff = fElectronCoeff.GetCoefficient(energy); break;
+        case -11:
+            coeff = fPositronCoeff.GetCoefficient(energy); break;
+        case 2212:
+            coeff = fProtonCoeff.GetCoefficient(energy); break;
+        case 2112:
+            coeff = fNeutronCoeff.GetCoefficient(energy); break;
+        default:
+            G4ExceptionDescription ED;
+            ED << "No conversion coefficients available for " << pid << "(" << name << ")" << G4endl;
+            G4Exception("PSEquivalentDose::ProcessHits","Whoopsa",JustWarning,ED);
+    }
+
 
     G4int idx = ((G4TouchableHistory*)
             (pre_step->GetTouchable()))->GetReplicaNumber(indexDepth);
     G4double cubicVolume = ComputeVolume(aStep, idx);
+    G4double flux = stepLength / cubicVolume;
 
-    G4double CellFlux = stepLength / cubicVolume;
-    if (weighted) CellFlux *= pre_step->GetWeight();
+    G4double CellEqDose = flux * coeff;
+    if (weighted) CellEqDose *= pre_step->GetWeight();
     G4int index = GetIndex(aStep);
-    EvtMap->add(index, CellFlux);
+    EvtMap->add(index, CellEqDose);
 
-    G4cout << "PDG:" << aStep->GetTrack()->GetParticleDefinition()->GetPDGEncoding() << " FLUX: " << CellFlux << " ENE: " << pre_step->GetKineticEnergy() << " C: " << coeff  << G4endl;
+    //G4cout << "PDG:" << aStep->GetTrack()->GetParticleDefinition()->GetPDGEncoding() << " EqDose: " << CellEqDose << " ENE: " << pre_step->GetKineticEnergy() << " C: " << coeff  << G4endl;
 
     return TRUE;
 
 }
 
-void PSEquivalntDose::clear() {};
+void PSEquivalntDose::clear() {}
 
-void PSEquivalntDose::DrawAll() {};
+void PSEquivalntDose::DrawAll() {}
 
-void PSEquivalntDose::PrintAll() {};
+void PSEquivalntDose::PrintAll() {}
 
 G4double PSEquivalntDose::ComputeVolume(G4Step* aStep, G4int idx){
 
