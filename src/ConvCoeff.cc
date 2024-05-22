601681af7e31c1b21a9cfff4603d72b53846decb
maluethi <maluethi@protonmail.ch>
2024-05-22 16:59:24 +0200

    add handling of different particles

--- src/ConvCoeff.cc
+++ src/ConvCoeff.cc
@@ -4,6 +4,8 @@
 
 #include <fstream>
 #include <G4ios.hh>
+#include <limits>
+#include <vector>
 #include "G4Exception.hh"
 #include "ConvCoeff.hh"
 
@@ -19,12 +21,13 @@ ConvCoeff::ConvCoeff(G4String ParticleName, G4String Geometry, G4String Filepath
 ConvCoeff::~ConvCoeff() {;}
 
 G4double ConvCoeff::GetCoefficient(G4double energy) {
-    auto up = fCoefficients.upper_bound(energy);    // point 2 first: Energy [MeV], second: Coeff [Sv/cm²]
+    auto up = fCoefficients.lower_bound(energy);    // point 2 first: Energy [MeV], second: Coeff [Sv/cm²]
 
     if (up == fCoefficients.end()) {
         G4ExceptionDescription ED;
         ED << "Energy out of range for Coeefficients: " << fParticleName << " and Energy: " << energy << G4endl;
         G4Exception("ConvCoeff::GetCoefficient","Just one Ping",JustWarning,ED);
+        return 0.;
     }
 
     auto low = std::prev(up);   // point before
@@ -36,26 +39,57 @@ G4double ConvCoeff::GetCoefficient(G4double energy) {
 
     auto m = dC / dE;
     auto n = (low->second * up->first - up->second * low->first) / dE;
-
-    return m * energy + n;
+    auto interp = m * energy + n;
+    return interp;
 }
 
 
 void ConvCoeff::ReadFile() {
     G4String line;
-    std::ifstream myfile (fDataDir + "/Protons_Effective dose.txt");
-    while (std::getline(myfile, line))
-    {
-        myfile.ignore(3, '\n');
+    G4String filename = fDataDir + "/" + fParticleName + "_Effective dose.txt";
 
+    std::ifstream input (filename);
+
+    if (!input.good()) {
+        G4ExceptionDescription ED;
+        ED << "File does not exist: " << filename  << G4endl;
+        G4Exception("ConvCoeff::ReadFile","BeepBuup",RunMustBeAborted,ED);
+    }
+
+    G4cout << "Reading from: " << filename << G4endl;
+
+    G4int n_skip = 2;
+    G4int idx = 0;
+
+    std::vector<G4String> col;
+
+    fCoefficients.insert(std::pair<G4double, G4double>(0, 0));
+
+    while (std::getline(input, line))
+    {
+        if (idx < n_skip) {
+            idx++;
+            continue;
+        }
+        if (idx == 2) {
+            //TODO: Handle Header
+            idx++;
+            continue;
+        }
+        idx++;
         std::istringstream iss(line);
         G4double ene;
         G4double AP;
 
         iss >> ene >> AP;
 
-        fCoefficients.insert(std::pair<G4double, G4double >(ene, AP));
-
+        if (input.fail()) {
+            input.clear();
+            input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else {
+            fCoefficients.insert(std::pair<G4double, G4double>(ene, AP));
+        }
         // process pair (a,b)
     }
 
